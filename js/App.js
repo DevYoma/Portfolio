6df9d382de309df1b92e54d04b6dd30c1f355d4c
DevYoma <Lawrenceyoma@gmail.com>
2021-01-15 00:26:34 +0100

    finishing touches

--- js/App.js
+++ js/App.js
@@ -5,3 +5,12 @@ let navToggler = document.querySelector('.social__nav__toggler');
 navToggler.addEventListener('click', function(){
     socialNav.classList.toggle('active');
 });
+
+
+function myTwitter(){
+    location.href = "https://twitter.com/devYoma";
+}
+
+function myLinkedIn(){
+    location.href = "https://www.linkedin.com/in/ogheneyoma-emore-87a8341b3";
+}
